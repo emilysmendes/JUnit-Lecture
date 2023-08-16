6a0408dbe8a33bdb56d8be0ff1f4dd72e66b7d9e
Emily Svetcos <emilysvetcos@gmail.com>
2023-08-16 11:48:02 -0500

    finished testing

--- src/main/java/Coffee.java
+++ src/main/java/Coffee.java
@@ -1,2 +1,40 @@
 public class Coffee {
+    private String name;
+    private String roast;
+    private double price;
+
+    public Coffee() {}
+
+    public Coffee(String name, String roast, double price) {
+        this.name = name;
+        this.roast = roast;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getRoast() {
+        return this.roast;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRoast(String roast) {
+        this.roast = roast;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
 }
+
--- src/test/java/CoffeeTest.java
+++ src/test/java/CoffeeTest.java
@@ -1,9 +1,17 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CoffeeTest {
     private Coffee emptyCoffee;
     private Coffee actualCoffee;
+
+    private double deltaValue = 0.000001;
+
+    @Before
+    public void setup() {
+        actualCoffee = new Coffee ("The Bean-inator", "Espresso", 19.99);
+    }
 
     @Test
     public void testCoffeeConstructor() {
@@ -13,7 +21,36 @@ public class CoffeeTest {
         assertNull(emptyCoffee);
 
         //asser that actualCoffee is NOT null
-        assertNull(actualCoffee);
+        assertNotNull(actualCoffee);
+    }
+
+    @Test
+    public void testCoffeeThreeArgConstructor () {
+        actualCoffee = new Coffee ("The Bean-inator", "Espresso", 19.99);
+
+        assertNotNull(actualCoffee);
+
+    }
+
+    @Test
+    public void testCoffeeGetters() {
+        assertEquals(actualCoffee.getName(), "The Bean-inator");
+        assertEquals(actualCoffee.getRoast(), "Espresso");
+        assertEquals(actualCoffee.getPrice(), 19.99, deltaValue);
+    }
+
+    @Test
+    public void testCoffeeSetters() {
+        actualCoffee.setName("The Bean-inator 2");
+        assertEquals(actualCoffee.getName(), "The Bean-inator 2");
+
+        actualCoffee.setRoast("dark");
+        assertEquals(actualCoffee.getRoast(), "dark");
+
+        actualCoffee.setPrice(6.49);
+        assertEquals(actualCoffee.getPrice(), 6.49, deltaValue);
+
+
     }
 
 
